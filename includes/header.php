400436b5da6cf07bd296e042197946f38003ee67
glob8076 <glob8076@gmail.com>
2025-12-03 14:11:38 -0800

    Update header.php
    Added CSS

--- includes/header.php
+++ includes/header.php
@@ -1,17 +1,20 @@
 <?php
 session_start();
+echo "<link rel='stylesheet' type='text/css' href='styles.css' />";
 ?>
-<nav>
-    <a href="index.php">Home</a> |
-    <?php
-    if (isset($_SESSION['user_id'])) {
-        echo '<a href="cart.php">Cart</a> | <a href="logout.php">Logout</a> | ';
-        if($_SESSION['is_admin'] == 1){
-            echo '<a href="admin.php">Admin Panel</a>';
+<header>
+    <div class='navbar'>
+        <a class="nav-links" href="index.php">Home</a> |
+        <?php
+        if (isset($_SESSION['user_id'])) {
+            echo '<a class="nav-links" href="cart.php">Cart</a> | <a class="nav-links" href="logout.php">Logout</a> | ';
+            if($_SESSION['is_admin'] == 1){
+                echo '<a class="nav-links" href="admin.php">Admin Panel</a>';
+            }
+        } else {
+            echo '<a class="nav-links" href="login.php">Login</a> | <a class="nav-links" href="register.php">Register</a>';
         }
-    } else {
-        echo '<a href="login.php">Login</a> | <a href="register.php">Register</a>';
-    }
-    ?>
-</nav>
+        ?>
+    </div>
+</header>
 <hr>
